509ef14de049d2fb4e8036e6a4d18c1a4b984f2d
大空スバル大好き <111629702+BlueBoy247@users.noreply.github.com>
2024-03-23 16:17:04 +0800

    Modified on 2024.03.23
    更新內容
    [car.ino]
    1. 原先的作法是路由器當作存取點，ESP32當作站點，現在改成ESP32直接當作存取點。
    2. 縮短觸發timeout的時間。
    3. 新增控制伺服馬達的程式碼，並完成當收到車子左轉及右轉指令時所需執行的內容。
    4. 將發送網頁的程式碼獨立成為page()函式，並增加redirect()函式，讓controller在傳送指令後能回到主頁面。
    [controller.html]
    1. 新增查看車子目前狀態的區塊。
    2. 增加自動重新整理頁面的功能，若開啟此功能，在多人同時遙控車子時就能保持更新車子目前狀態。
    3. 修改controller介面排版。
    
    待修正/解決事項：
    1. controller傳送指令後，無論原先是否有啟用自動重新整理頁面功能，都會因為重定向而被重設為啟用。
    
    Update
    [car.ino]
    1. The original approach was to use the router as an access point with the ESP32 as a station. Now, it is changed to use the ESP32 directly as the access point.
    2. Shortened the timeout  time.
    3. Added code to control servo motors, and completed the actions to be taken when receiving commands for left and right turns of the car.
    4. Extracted the code for sending web pages into a separate function called page(), and added a redirect() function to allow the controller to return to the main page after sending commands.
    
    [controller.html]
    1. Added a section to view the current status of the car.
    2. Implemented automatic page refreshing functionality. If this feature is enabled, the page will update the current status of the car every 5 seconds, useful for multiple users controlling the car simultaneously.
    3. Modified the layout of the controller interface.
    
    To be fixed/resolved:
    1. After sending commands from the controller, regardless of whether the automatic page refreshing feature was enabled previously, it will be reset to enabled due to redirection.

--- car.ino
+++ car.ino
@@ -1,30 +1,38 @@
 #include <WiFi.h>
+#include <Servo.h>
 
-// WiFi information
-char ssid[] = "";
-char password[] = "";
+// AP information
+const char* ssid = "醬炒鴨肉🦆"; // Custom SSID
+const char* password = "otterhenry"; //Custom password
+const int channel = 1; // Wi-Fi channel number (1-13)
+const int ssid_hidden = 0; // 0 = broadcast SSID, 1 = hide SSID
+const int max_connection = 4; // Maximum simultaneous connected clients (1-4)
 
-// Setting webserver port number
+// Setting web server port number
 WiFiServer server(80);
 
-// Variable to store the HTTP request
-String header;
-
-// Motor A
+// Motor A (Front-wheel)
 int motor1Pin1 = 16;
 int motor1Pin2 = 17;
-// Motor B
+// Motor B (Rear-wheel)
 int motor2Pin1 = 18;
 int motor2Pin2 = 19;
+// Servo
+Servo servoTurn;
+int servoPin = 25;
+int servoPos = 0;
 
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0; 
-// Define timeout time
-const long timeoutTime = 2000;
+String header; // Variable to store the HTTP request
+String state = "Stop"; // Variable to store the current car state
+
+unsigned long curT = millis(); // Current time
+unsigned long prevT = 0; // Previous time
+const long timeout = 500; // Define timeout time
 
 void setup(){
+  // Start
+  Serial.begin(115200);
+
   // Set the pins as outputs
   pinMode(motor1Pin1, OUTPUT);
   pinMode(motor1Pin2, OUTPUT);
@@ -37,23 +45,16 @@ void setup(){
   digitalWrite(motor2Pin1, LOW);
   digitalWrite(motor2Pin2, LOW);
   
-  // Start
-  Serial.begin(115200);
-  Serial.println("Testing DC Motor...");
+  // Set servo pin
+  servoTurn.attach(servoPin);
 
-  // Wifi connection
-  Serial.print("Connecting to: ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while(WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println("");
-  Serial.println("Connected. IP address: ");
-  Serial.println(WiFi.localIP());
+  // Setting AP
+  Serial.println("Setting AP......");
+  WiFi.softAP(ssid, password, channel, ssid_hidden, max_connection);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("Succeeded. AP IP address: ");
+  Serial.println(IP);
   server.begin();
-
 }
 
 void loop(){
@@ -61,18 +62,17 @@ void loop(){
   WiFiClient client = server.available();
   
   if(client){
-    currentTime = millis();
-    previousTime = currentTime;
+    curT = millis();
+    prevT = curT;
     Serial.println("New Client.");
     String currentLine = "";
     
-    while(client.connected() && currentTime - previousTime <= timeoutTime){
-      currentTime = millis();
+    while(client.connected() && curT - prevT <= timeout){
+      curT = millis();
       
       // If there's bytes to read from the client,
       if(client.available()){
         char c = client.read(); // Read a byte, then
-        Serial.write(c); // Print it out the serial monitor
         header += c;
         
         // If the byte is a newline character
@@ -80,60 +80,56 @@ void loop(){
         if(c == '\n'){
           // If the current line is blank, you got two newline characters in a row.
           if(currentLine.length() == 0){
-            // HTTP headers
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
             // Command from client
             if(header.indexOf("GET /S") >= 0){
               Serial.println("Motor Stop");
+              state = "Stop";
               digitalWrite(motor1Pin1, LOW);
               digitalWrite(motor1Pin2, LOW);
               digitalWrite(motor2Pin1, LOW);
               digitalWrite(motor2Pin2, LOW);
+              redirect(client);
             }
             else if(header.indexOf("GET /F") >= 0){
               Serial.println("Moving Forward");
+              state = "Forward";
               digitalWrite(motor1Pin1, HIGH);
               digitalWrite(motor1Pin2, LOW);
               digitalWrite(motor2Pin1, HIGH);
               digitalWrite(motor2Pin2, LOW);
+              redirect(client);
             }
             else if(header.indexOf("GET /B") >= 0){
               Serial.println("Moving Backwards");
+              state = "Backwards";
               digitalWrite(motor1Pin1, LOW);
               digitalWrite(motor1Pin2, HIGH);
               digitalWrite(motor2Pin1, LOW);
               digitalWrite(motor2Pin2, HIGH);
+              redirect(client);
             }
             else if(header.indexOf("GET /L") >= 0){
               Serial.println("Turn Left");
+              state = "Left";
+              for(int i = 0; i < 15; i++) { 
+                servoTurn.write(--servoPos); 
+                delay(10); 
+              }
+              redirect(client);
             }
             else if(header.indexOf("GET /R") >= 0){
               Serial.println("Turn Right");
+              state = "Right";
+              for(int i = 0; i < 15; i++) { 
+                servoTurn.write(++servoPos); 
+                delay(10); 
+              }
+              redirect(client);
             }
-            
-            // Website design
-            client.println("<!DOCTYPE html><html lang=\"en\">");
-            client.println("<head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
-            client.println("<link rel=\"icon\" href=\"data:,\"><title>Controller</title>");
-            client.println("<style>h1{text-align: center;}");
-            client.println(".buttondiv{display: grid;grid-template-columns: repeat(3,1fr);gap: 5px;}");
-            client.println(".button{background-color: black;border: none;color: white;padding: 16px 40px;text-decoration: none;font-size: 30px;cursor: pointer;width: 90%;}");
-            client.println(".switch{background-color: red;border: none;color: white;padding: 16px 40px;text-decoration: none;font-size: 30px;cursor: pointer;width: 90%;}");
-            client.println("</style></head>");
-            client.println("<body><h1>Car Controller</h1><div class=\"buttondiv\">");
-            client.println("<p></p><a href=\"/F\"><button class=\"button\">↑</button></a><p></p>");
-            client.println("<a href=\"/L\"><button class=\"button\">←</button></a>");
-            client.println("<a href=\"/S\"><button class=\"switch\">■</button></a>");
-            client.println("<a href=\"/R\"><button class=\"button\">→</button></a>");
-            client.println("<p></p><a href=\"/B\"><button class=\"button\">↓</button></a><p></p>");
-            client.println("</div></body></html>");
+            else{
+              page(client);
+            }
 
-            // The HTTP response ends with another blank line
-            client.println();
             // Break out of the while loop
             break;
           }
@@ -156,4 +152,42 @@ void loop(){
     Serial.println("Client disconnected.");
     Serial.println("");
   }
+}
+
+void page(WiFiClient client){
+  // HTTP headers
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-type:text/html");
+  client.println("Connection: close");
+  client.println();
+
+  // Controller design
+  client.println("<!DOCTYPE html><html lang=\"en\"><head><title>Controller</title>");
+  client.println("<meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+  client.println("<style>h1, h2{text-align: center;}.red{background-color: red;}.black{background-color: black;}");
+  client.println(".button{border: none;color: white;padding: 16px 0;text-decoration: none;font-size: 30px;width: 100%;height: 100%;}");
+  client.println(".buttondiv{display: grid;grid-template-columns: repeat(3,1fr);gap: 5px;}</style></head>");
+  client.println("<body><h1>Car Controller</h1><div class=\"buttondiv\">");
+  client.println("<p></p><a href=\"/F\"><button class=\"button black\">↑</button></a><p></p>");
+  client.println("<a href=\"/L\"><button class=\"button black\">←</button></a>");
+  client.println("<a href=\"/S\"><button class=\"button red\">Stop</button></a>");
+  client.println("<a href=\"/R\"><button class=\"button black\">→</button></a>");
+  client.println("<p></p><a href=\"/B\"><button class=\"button black\">↓</button></a><p></p>");
+  client.println("</div><h2>Current State: "+state+"</h2>");
+  client.println("<input id=\"refresh\" type=\"checkbox\" onchange=\"check(this);\" checked><label for=\"refresh\">Auto Refresh</label>");
+  client.println("<script>const checkbox = document.getElementById(\"refresh\"); var interval;function update(){location.reload();}");
+  client.println("function check(e){if(e.checked){interval = setInterval(update, 5000);} else{clearInterval(interval);}}");
+  client.println("checkbox.onchange();</script></body></html>");
+
+  // The HTTP response ends with another blank line
+  client.println();
+}
+
+void redirect(WiFiClient client){
+  String url = "http://192.168.4.1/";
+  // HTTP headers
+  client.println("HTTP/1.1 302 Found");
+  client.println("Location: " + url);
+  client.println("Connection: close");
+  client.println();
 }
